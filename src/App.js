3d917eaf11e087db7b7fba8305045a199739fe50
djoun52 <simon.rioux.trv@gmail.com>
2021-12-16 08:43:46 +0100

    fin number 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Pendu from "./Components/pendu/pendu";
+
+function App() {
+  return (
+
+    <div id="wrapper">
+      <h1>mon app</h1>
+      <Pendu />
+    </div>
+  );
+}
+
+export default App;
